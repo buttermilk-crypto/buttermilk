ef8e2c991c01dce1e1f2daef07353841d50c4ded
Dave <Dave@0d64cb13-aa91-49db-97c5-347823402cb2>
2016-01-10 05:20:13 +0000

    continued refinement on dialogs

--- trunk/buttermilk-core/src/main/java/com/cryptoregistry/formats/MapDataFormatter.java
+++ trunk/buttermilk-core/src/main/java/com/cryptoregistry/formats/MapDataFormatter.java
@@ -46,4 +46,64 @@ public class MapDataFormatter {
 			g.writeEndObject();
 		}
 	}
+	
+	/**
+	 * format an output like this:
+	 * 
+	 * "uuid0": {
+	 * 		"Key0": "Item0",
+	 *		"Key1: "Item1"
+	 * },
+	 * "uuid1": {
+	 * 		"Key0": "Item0",
+	 *		"Key1: "Item1"
+	 * }
+	 * ...
+	 * 
+	 * @return
+	 */
+	public String formatAsFragment(){
+		StringBuffer buf = new StringBuffer();
+		int overallCount = 1;
+		for(MapData data: mapData){
+			buf.append(quote(data.uuid));
+			buf.append(": {\n");
+			int size = data.data.size();
+			int count = 1;
+			Iterator<String> iter = data.data.keySet().iterator();
+			while(iter.hasNext()){
+				String key = iter.next();
+				String value = data.data.get(key);
+				buf.append("          ");
+				if(count < size) buf.append(keyValuePair(key, value, true));
+				else buf.append(keyValuePair(key, value, false));
+				count++;
+			}
+			if(overallCount<mapData.size()) buf.append("},");
+			else  buf.append("     }");
+			overallCount++;
+		}
+		return buf.toString();
+	}
+	
+	private String quote(String in){
+		StringBuffer buf = new StringBuffer();
+		buf.append(QUOTE);
+		buf.append(in);
+		buf.append(QUOTE);
+		return buf.toString();
+	}
+	
+	private String keyValuePair(String key, String value, boolean comma){
+		StringBuffer buf = new StringBuffer();
+		buf.append(quote(key));
+		buf.append(":");
+		buf.append(quote(value));
+		if(comma) buf.append(",");
+		buf.append("\n");
+		return buf.toString();
+	}
+	
+	private static final char QUOTE = '"';
+	
 }
