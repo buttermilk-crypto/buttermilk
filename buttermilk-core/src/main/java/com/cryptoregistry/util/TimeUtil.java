ee831fceb3a2a1875a7ba18bf38c69a26acfebe1
Dave <Dave@0d64cb13-aa91-49db-97c5-347823402cb2>
2016-02-06 11:11:50 +0000

    Fixed TimeUtil to work correctly; added test

--- buttermilk-core/src/main/java/com/cryptoregistry/util/TimeUtil.java
+++ buttermilk-core/src/main/java/com/cryptoregistry/util/TimeUtil.java
@@ -5,16 +5,16 @@
  */
 package com.cryptoregistry.util;
 
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
+
+import java.util.Calendar;
 import java.util.Date;
-import java.util.TimeZone;
+import java.util.GregorianCalendar;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 /**<pre>
- * Used for ISO 8601 formatting, which is our standard for date-time values
+ * Used for ISO 8601 formatting, which is our standard for date-time values. Reworked as per 
+ * http://stackoverflow.com/questions/2201925/converting-iso-8601-compliant-string-to-java-util-date
  *
  * </pre>
  * @author Dave
@@ -25,32 +25,22 @@ public class TimeUtil {
 	private static Lock lock = new ReentrantLock();
 
 	public static String format(Date date) {
-		return getDateFormat().format(date);
+		 lock.lock(); 
+		  try {
+			  Calendar c = GregorianCalendar.getInstance();
+			  c.setTime(date);
+			  return javax.xml.bind.DatatypeConverter.printDateTime(c);
+		 } finally {
+		     lock.unlock();
+		 }
 	}
-	/**
-	 * Returns a formatter for ISO 8601 format as described at http://www.cl.cam.ac.uk/~mgk25/iso-time.html
-	 * @return
-	 */
-	public static final DateFormat getDateFormat() {
-		  lock.lock(); 
-		     try {
-		    	 TimeZone tz = TimeZone.getTimeZone("UTC");
-		 		 DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ");
-		 		 df.setTimeZone(tz);
-		 		 return df;
-		     } finally {
-		       lock.unlock();
-		     }
-	}
+	
 	
 	public static final Date getISO8601FormatDate(String in) {
 		 lock.lock(); 
 	     try {
-	    	 try {
-	 			return getDateFormat().parse(in);
-	 		} catch (ParseException e) {
-	 			throw new RuntimeException("Date failed to parse as ISO 8601: "+in);
-	 		}
+	    	 Calendar cal = javax.xml.bind.DatatypeConverter.parseDateTime(in);
+	    	 return cal.getTime();
 	     } finally {
 	       lock.unlock();
 	     }
@@ -59,7 +49,7 @@ public class TimeUtil {
 	public static final String now() {
 		 lock.lock(); 
 	     try {
-	    	 return getDateFormat().format(new Date());
+	    	return format(new Date());
 	     } finally {
 	       lock.unlock();
 	     }
