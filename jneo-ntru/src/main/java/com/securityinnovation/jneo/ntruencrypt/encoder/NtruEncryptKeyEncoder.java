b106309bb0b0688133e14f3b6fb8059a3486430f
Dave <Dave@0d64cb13-aa91-49db-97c5-347823402cb2>
2016-03-18 10:49:03 +0000

    Initial import.

---
+++ jneo-ntru/src/main/java/com/securityinnovation/jneo/ntruencrypt/encoder/NtruEncryptKeyEncoder.java
@@ -0,0 +1,46 @@
+/******************************************************************************
+ * NTRU Cryptography Reference Source Code
+ * Copyright (c) 2009-2013, by Security Innovation, Inc. All rights reserved.
+ *
+ * Copyright (C) 2009-2013  Security Innovation
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *********************************************************************************/
+
+package com.securityinnovation.jneo.ntruencrypt.encoder;
+
+import com.securityinnovation.jneo.math.FullPolynomial;
+import com.securityinnovation.jneo.ntruencrypt.KeyParams;
+import com.securityinnovation.jneo.FormatNotSupportedException;
+import com.securityinnovation.jneo.ParamSetNotSupportedException;
+
+public interface NtruEncryptKeyEncoder {
+	/**
+	 * Encode a public key as a byte array.
+	 */
+	public byte[] encodePubKey(KeyParams keyParams, FullPolynomial h);
+
+	/**
+	 * Encode a private key as a byte array.
+	 */
+	public byte[] encodePrivKey(KeyParams keyParams, FullPolynomial h,
+			FullPolynomial f);
+
+	/**
+	 * Parse a public or private key blob.
+	 */
+	public RawKeyData decodeKeyBlob(byte keyBlob[])
+			throws FormatNotSupportedException, ParamSetNotSupportedException;
+}
