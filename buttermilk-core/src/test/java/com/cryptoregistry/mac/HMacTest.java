eb02d5a1b67a41a732ede3c9b2fe3fdf2064609f
Dave <Dave@0d64cb13-aa91-49db-97c5-347823402cb2>
2016-03-28 08:51:00 +0000

    fixed test

--- buttermilk-core/src/test/java/com/cryptoregistry/mac/HMacTest.java
+++ buttermilk-core/src/test/java/com/cryptoregistry/mac/HMacTest.java
@@ -3,11 +3,13 @@ package com.cryptoregistry.mac;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
+
+import junit.framework.Assert;
 
 import org.junit.Test;
 
 import net.iharder.Base64;
+
 import org.bouncycastle.crypto.Digest;
 import org.bouncycastle.crypto.digests.SHA256Digest;
 import org.bouncycastle.crypto.macs.HMac;
@@ -19,14 +21,16 @@ public class HMacTest {
 	@Test
 	public void test0() throws NoSuchAlgorithmException {
 		
-		SecureRandom rand = new SecureRandom();
-		byte [] key = new byte [128];
-		rand.nextBytes(key);
+		//SecureRandom rand = new SecureRandom();
+		//byte [] key = new byte [128];
+		//rand.nextBytes(key);
+		
+		byte [] key = "password".getBytes(StandardCharsets.UTF_8);
 		
 		byte [] mac = hmac("My Test String", key, new SHA256Digest());
 		try {
 			String macString = Base64.encodeBytes(mac, Base64.URL_SAFE);
-			System.err.println(macString);
+			Assert.assertEquals("FNhfPHjYNywIexKRXzme6TmQ7CXm6_sLc-skeWFMEYY=", macString);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
